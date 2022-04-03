b9731f0350cdb1cbd53a012794bd6a0139c12030
MIDU-SER <98685114+MIDU-SER@users.noreply.github.com>
2022-04-03 15:34:39 +0530

    update StringSession.js

--- minnutty/StringSession.js
+++ minnutty/StringSession.js
@@ -18,7 +18,7 @@ class StringSession {
 
     deCrypt(string = undefined) {
 
-        if ('MISSHELENA_CODE' in process.env && string === undefined) {
+        if ('MINNU_CODE' in process.env && string === undefined) {
 
             string = process.env.STRING_SESSION;
 
@@ -46,7 +46,7 @@ class StringSession {
 
     createStringSession(dict) {
 
-        return 'MISSHELENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'MINNU;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
 
     }
 
